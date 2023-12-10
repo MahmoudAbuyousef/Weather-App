830167838b8a914683f8723c0bd833b63e3fdd7d
Mahmoud Abu Youssef <mahmoudabuyoussef20@gmail.com>
2023-12-10 10:02:50 +0200

    test

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -7,7 +7,7 @@ interface InputProps {
 function Input({ handelSearch, setLocation }: InputProps) {
   return (
     <div className="input_container flex items-center md:w-2/4 w-full  rounded-lg p-3 mt-10 md:mt-0 ">
-      <input type="text" placeholder="Search City" className="input_feild w-full bg-transparent outline-none text-white" onKeyDown={handelSearch} onChange={(e) => setLocation(e.target.value)} />
+      <input type="text" placeholder="Search City" className="input_feild w-full bg-transparent outline-none text-white" onKeyDown={handelSearch} onChange={(e) => setLocation(e.target.value)} />{" "}
     </div>
   );
 }
